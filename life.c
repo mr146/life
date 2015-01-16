a6c59cedb5e4ccad645ba83e48a473d4b5d4eede
mv146 <mv146@skbkontur.ru>
2015-01-16 06:37:10 +0500

    compilation (WAT?) and memory leak fixes, time measurement

--- life.c
+++ life.c
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
@@ -17,7 +18,8 @@ int toindex(int row, int col, int N) {
 }
 
 void printgrid(char* grid, char* buf, FILE* f, int N) {
-    for (int i = 0; i < N; ++i) {
+    int i;
+    for (i = 0; i < N; ++i) {
         strncpy(buf, grid + i * N + N, N);
         buf[N] = 0;
         fprintf(f, "%s\n", buf);
@@ -26,6 +28,8 @@ void printgrid(char* grid, char* buf, FILE* f, int N) {
 
 void master(int size, int argc, char* argv[])
 {
+//    fprintf(stderr, "i am master");
+//    return;
     if (argc != 5) {
         fprintf(stderr, "Usage: %s N input_file iterations output_file\n", argv[0]);
         return;
@@ -33,7 +37,8 @@ void master(int size, int argc, char* argv[])
     int N = atoi(argv[1]); // grid size
     int iterations = atoi(argv[3]);
     int* lines = (int*) malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++)
+    int i;
+    for (i = 0; i < size; i++)
     {
         lines[i] = N / size;
         if(i < N % size)
@@ -42,11 +47,11 @@ void master(int size, int argc, char* argv[])
     FILE* input = fopen(argv[2], "r");
     char* grid = (char*) malloc((N + 2) * N * sizeof(char));
     char* buf = (char*) malloc(N * N * sizeof(char));
-    for (int i = 0; i < N; ++i) {
+    for (i = 0; i < N; ++i) {
         fscanf(input, "%s", grid + i * N + N);
     }
     fclose(input);
-    for(int i = 0; i < N; i++)
+    for(i = 0; i < N; i++)
     {
         grid[i] = grid[N * N + i];
         grid[N * (N + 1) + i] = grid[N + i];
@@ -54,10 +59,14 @@ void master(int size, int argc, char* argv[])
 
     MPI_Request* reqs = (MPI_Request*)malloc(size * sizeof(MPI_Request));
     MPI_Status* stats = (MPI_Status*)malloc(size * sizeof(MPI_Status));
-    for(int iter = 0; iter < iterations; iter++)
+    int iter;
+    struct timeval tval_before;
+    gettimeofday(&tval_before, NULL);
+    for(iter = 0; iter < iterations; iter++)
     {
         int sent = 0;
-        for(int sl = 0; sl < size; sl++)
+        int sl;
+        for(sl = 0; sl < size; sl++)
         {
             int cur_pntr = sent * N;
             MPI_Isend(grid + cur_pntr, (lines[sl] + 2) * N, MPI_CHARACTER, sl + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &reqs[sl]);
@@ -65,32 +74,39 @@ void master(int size, int argc, char* argv[])
         }
         MPI_Waitall(size, reqs, stats);
         int received = 0;
-        for(int sl = 0; sl < size; sl++)
+        for(sl = 0; sl < size; sl++)
         {
             int cur_pntr = received * N;
-            MPI_Irecv(buf + cur_pntr, lines[sl] * N, MPI_CHARACTER, sl + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &reqs[sl]);
+            MPI_Irecv(grid + cur_pntr, lines[sl] * N, MPI_CHARACTER, sl + 1, MPI_ANY_TAG, MPI_COMM_WORLD, &reqs[sl]);
             received += lines[sl];
         }
         MPI_Waitall(size, reqs, stats);
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < N; j++)
-                grid[i * N + j + N] = buf[i * N + j];
-        for(int i = 0; i < N; i++)
+        int i, j;
+        for(i = 0; i < N; i++)
         {
             grid[i] = grid[N * N + i];
             grid[N * (N + 1) + i] = grid[N + i];
         }
     }
+    struct timeval tval_after, tval_result;
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    fprintf(stderr, "Took %d ms\n", tval_result.tv_sec * 1000 + tval_result.tv_usec / 100);
     FILE* output = fopen(argv[4], "w");
     printgrid(grid, buf, output, N);
     fclose(output);
     free(grid);
     free(buf);
+    free(reqs);
+    free(stats);
+    free(lines);
     return;
 }
 
 void slave(int rank, int size, int argc, char* argv[])
 {
+ //   fprintf(stderr, "i am slave %d\n", rank);
+ //   return;
     if (argc != 5) {
         return;
     }
@@ -102,18 +118,19 @@ void slave(int rank, int size, int argc, char* argv[])
         lines++;
     char* buf = (char*) malloc(N * (lines + 2) * sizeof(char));
     char* inc = (char*) malloc(N * (lines + 2) * sizeof(char));
-    for(int iter = 0; iter < iterations; iter++)
+    int iter, i, j, di, dj;
+    for(iter = 0; iter < iterations; iter++)
     {
         MPI_Status stat;
         MPI_Request req;
         MPI_Irecv(inc, (lines + 2) * N, MPI_CHARACTER, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
         MPI_Wait(&req, &stat);
-        for(int i = 1; i <= lines; i++)
-            for(int j = 0; j < N; j++)
+        for(i = 1; i <= lines; i++)
+            for(j = 0; j < N; j++)
             {
                 int alive_count = 0;
-                for (int di = -1; di <= 1; ++di) {
-                    for (int dj = -1; dj <= 1; ++dj) {
+                for (di = -1; di <= 1; ++di) {
+                    for (dj = -1; dj <= 1; ++dj) {
                         if ((di != 0 || dj != 0) && inc[toindex(i + di, j + dj, N)] == ALIVE) {
                             ++alive_count;
                         }
@@ -130,6 +147,7 @@ void slave(int rank, int size, int argc, char* argv[])
         MPI_Wait(&req, &stat);
     }
     free(buf);
+    free(inc);
 }
 
 int main(int argc, char* argv[]) {
